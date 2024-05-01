c0d4d1af25ed6020f88dfbaa5e701c8bae8e2f1c
Ishan Jain <jainishan18@gmail.com>
2024-05-01 17:05:48 +0530

    made changes

--- tmbd/src/SearchBar.js
+++ tmbd/src/SearchBar.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function SearchBar({onSearch}) {
-  const  [query, setQuery] = React.useState('')
-  const handleSearch =()=>{
-    onSearch(query)
-  }
+export default function SearchBar({ onSearch }) {
+  const [query, setQuery] = React.useState("");
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
   return (
-    <div className='search'>
-      <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)} />
+    <div className="search">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <button onClick={handleSearch}>Search</button>
     </div>
-  )
+  );
 }
--- tmbd/src/Tvlist.js
+++ tmbd/src/Tvlist.js
@@ -1,32 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar"; // Import SearchBar component
+import TvCard from "./TvCard";
 
-import "./App.css"
-import TvCard from './TvCard'
-export default function Tvlist({series}) {
-    const [sortOrder,setSortOrder] = useState('asc')
-    const sortedMovies = [...series].sort((a,b)=>{
-      if(sortOrder === "asc"){
-        return a.vote_count - b.vote_count
-      } else{
-        return b.vote_count - a.vote_count
-      }
-    })
-    const handleSort =()=>{
-        setSortOrder((prev)=>(prev === "asc"?"desc":"asc"))
+export default function TvList({ series }) {
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [filteredSeries, setFilteredSeries] = useState([]);
+
+  useEffect(() => {
+    setFilteredSeries(series);
+  }, [series]);
+
+  const handleSort = () => {
+    setSortOrder((prev) => (prev === "asc" ? "desc" : "asc"));
+  };
+
+  const handleSearch = (query) => {
+    const filtered = series.filter((tv) =>
+      tv.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredSeries(filtered);
+  };
+
+  const sortedSeries = [...filteredSeries].sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.vote_count - b.vote_count;
+    } else {
+      return b.vote_count - a.vote_count;
     }
+  });
+
   return (
-    <div >
-        <div>
-            <button onClick={handleSort}>
-                Sort by vote average ({sortOrder==="asc"?"Ascending":"Descending"})
-            </button>
-            
-        </div>
-        <ul className='movielist'>
-            {sortedMovies.map((tv)=>
-            <TvCard  key={tv.id} tv = {tv}/>)}
-        </ul>
-    
+    <div>
+      <div>
+        <button onClick={handleSort}>
+          Sort by vote average (
+          {sortOrder === "asc" ? "Ascending" : "Descending"})
+        </button>
+      </div>
+      <SearchBar onSearch={handleSearch} />
+      <ul className="movielist">
+        {sortedSeries.map((tv) => (
+          <TvCard key={tv.id} tv={tv} />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
